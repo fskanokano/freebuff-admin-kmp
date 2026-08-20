519d3a44a0ad5e2610c4631616f5f3ca50f8613e
fskanokano <fskanokano@users.noreply.github.com>
2026-08-21 03:41:17 +0800

    fix: correct Compose Multiplatform dependency coordinates

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,9 +47,8 @@ dependencies {
     implementation(project(":shared"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("org.jetbrains.compose:compose-uitoolkit:1.7.1")
-    implementation("org.jetbrains.compose:material3:1.7.1")
-    implementation("org.jetbrains.compose:components-resources:1.7.1")
+    implementation("org.jetbrains.compose.material3:material3:1.7.1")
+    implementation("org.jetbrains.compose.components:components-resources:1.7.1")
     implementation("io.ktor:ktor-client-android:3.0.3")
     implementation("io.ktor:ktor-client-okhttp:3.0.3")
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
             implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
             implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
             implementation("io.ktor:ktor-client-websockets:3.0.3")
-            implementation("org.jetbrains.compose.resources:resources:1.7.1")
+            implementation("org.jetbrains.compose.components:components-resources:1.7.1")
         }
         androidMain.dependencies {
             implementation("io.ktor:ktor-client-android:3.0.3")
